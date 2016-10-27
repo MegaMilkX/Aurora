6ae66262f688f402c22666d43dad31548ad19b21
megamilkx <gyrocopter3k@gmail.com>
2016-10-27 15:20:27 +0300

    Add window creation in Window constructor

--- src/window.cpp
+++ src/window.cpp
@@ -21,9 +21,54 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
     return 0;
 }
 
-Window::Window()
+Window::Window(const std::string& title, int width, int height)
+ : hWnd(NULL), msg({ 0 })
 {
+    std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+    std::wstring w_name = converter.from_bytes(title.c_str());
+
+    HINSTANCE hInstance = GetModuleHandle(0);
+
+    WNDCLASSEX wc;
     
+    wc.cbSize = sizeof(WNDCLASSEX);
+    wc.style = 0;
+    wc.lpfnWndProc = WndProc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = hInstance;
+    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW);
+    wc.lpszMenuName = NULL;
+    wc.lpszClassName = w_name.c_str();
+    wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+    
+    if(!RegisterClassEx(&wc))
+    {
+        return;
+    }
+
+	RECT rect;
+	rect.left = 0;
+	rect.top = 0;
+	rect.right = width;
+	rect.bottom = height;
+
+	AdjustWindowRectEx(&rect, WS_OVERLAPPEDWINDOW, false, 0);
+    
+    hWnd = CreateWindowExW(
+        0,
+        w_name.c_str(),
+        w_name.c_str(),
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, rect.right - rect.left, rect.bottom - rect.top,
+        NULL, NULL, hInstance, NULL);
+    
+    hWnd = hWnd;
+    msg = { 0 };
+
+    return;
 }
 
 Window::~Window()
@@ -32,7 +77,13 @@ Window::~Window()
 
 bool Window::Show()
 {
-
+    if(hWnd == NULL)
+        return false;
+    
+    ShowWindow(hWnd, 1);
+    UpdateWindow(hWnd);
+    
+    return true;
 }
 
 bool Window::Update()
