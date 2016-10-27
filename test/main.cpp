4ae9a88aac11cf9cfe3c0f4e377373006f345fb0
megamilkx <gyrocopter3k@gmail.com>
2016-10-27 15:20:36 +0300

    Update test project

--- test/main.cpp
+++ test/main.cpp
@@ -1,11 +1,9 @@
 #include <aurora/window.h>
-#include <iostream>
 
 int main()
 {
     Au::Window window;
-    
-    while(window.Update());
-    
+    if(window.Show())
+        while(window.Update());
     return 0;
 }
