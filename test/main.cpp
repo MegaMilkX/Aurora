cb7fb6a93f37ce4b3e663d815e4f0806ae575d6b
megamilkx <gyrocopter3k@gmail.com>
2016-11-10 01:47:57 +0300

    Update test code

--- test/main.cpp
+++ test/main.cpp
@@ -1,12 +1,15 @@
 #include <aurora/window.h>
-#include <aurora/gfx.h>
-#include <aurora/sfx.h>
-#include <aurora/input.h>
-#include <aurora/gamestate.h>
-
+//#include <aurora/gfx.h>
+//#include <aurora/sfx.h>
+//#include <aurora/input.h>
+//#include <aurora/gamestate.h>
+/*
 class InputHandler : public Au::Input::Handler
 {
 public:
+    InputHandler(const Au::Window& window)
+    : Au::Input::Handler(window) 
+    {}
     void OnKeyDown(const Au::Input::Event& e)
     {
         
@@ -14,17 +17,19 @@ public:
     
     void OnKeyUp(const Au::Input::Event& e)
     {
-        Au::GameState::OnInput(e);
+        
     }
 };
-
+*/
 int main()
 {
     Au::Window window;
+    Au::Window w2;
     
-    InputHandler input_handler(window);
+    //InputHandler input_handler(window);
     
-    if(window.Show())
-        while(window.Update());
+    if(window.Show() && w2.Show())
+        while(window.Update() || w2.Update());
+    
     return 0;
 }
