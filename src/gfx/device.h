16c3740993a68551f30752d6bc0b9585826482f5
megamilkx <gyrocopter3k@gmail.com>
2016-12-17 18:55:35 +0300

    Add GFX::Device

---
+++ src/gfx/device.h
@@ -0,0 +1,30 @@
+#ifndef GFXDEVICE_H
+#define GFXDEVICE_H
+
+#include "../window/window.h"
+#include "gl/glextutil.h"
+
+namespace Au{
+namespace GFX{
+
+class Device
+{
+public:
+    bool Init(Window& window);
+    void Cleanup();
+    
+    void Clear();
+    void SwapBuffers();
+    
+    int APIVersion();
+private:
+    HDC deviceContext;
+    HGLRC context;
+    HGLRC threadingContext;
+    int contextVersion = 0;
+};
+
+}
+}
+
+#endif
