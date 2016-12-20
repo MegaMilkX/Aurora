86566a6e1ccae2d9c62d277d06160cc154c9e89c
megamilkx <gyrocopter3k@gmail.com>
2016-12-21 02:24:53 +0300

    Add mesh header with some attribute placeholders

---
+++ src/gfx/mesh.h
@@ -0,0 +1,29 @@
+#ifndef MESH_H
+#define MESH_H
+
+namespace Au{
+namespace GFX{
+
+template<typename T, int count>
+class Attribute
+{
+public:
+    T& operator[](unsigned int index) { return value[index]; }
+private:
+    T value[count];
+}
+
+#define DEFINE_MESH_ATTRIB(NAME, TYPE, COUNT) \
+template<int LAYER> \
+class NAME : public Attribute<TYPE, COUNT> \
+{}
+
+DEFINE_MESH_ATTRIB(Position, float, 3);
+DEFINE_MESH_ATTRIB(Normal, float, 3);
+DEFINE_MESH_ATTRIB(ColorRGBA, char, 4);
+DEFINE_MESH_ATTRIB(ColorRGB, char, 3);
+
+}
+}
+
+#endif
