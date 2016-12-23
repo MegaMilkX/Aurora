2a5a1412857b4915fd70c6d769cb9b7588d15697
megamilkx <gyrocopter3k@gmail.com>
2016-12-24 00:49:47 +0300

    Add GLBuffer class

---
+++ src/gfx/gl/glbuffer.cpp
@@ -0,0 +1,40 @@
+#include "glbuffer.h"
+
+#include "glextutil.h"
+
+namespace Au{
+namespace GFX{
+
+GLBuffer GLBuffer::Create(unsigned int target, unsigned int usage_hint)
+{
+    GeometryBuffer gb;
+    gb.target = target;
+    gb.usage = usage_hint;
+    glGenBuffers(1, &gb.buffer);
+    return gb;
+}
+
+void GLBuffer::Destroy()
+{
+	glDeleteBuffers(1, &buffer);
+}
+
+bool GLBuffer::Data(void* data, size_t sz)
+{
+    glBindBuffer(target, buffer);
+    glBufferData(target, sz, data, usage);
+    return true;
+}
+
+bool GLBuffer::Valid()
+{
+    return buffer != 0;
+}
+
+void GLBuffer::Bind()
+{
+    glBindBuffer(target, buffer);
+}
+
+}
+}
---
+++ src/gfx/gl/glbuffer.h
@@ -0,0 +1,26 @@
+#ifndef GLBUFFER_H
+#define GLBUFFER_H
+
+namespace Au{
+namespace GFX{
+
+class GLBuffer
+{
+public:
+    GLBuffer() : buffer(0), target(0), usage(0) {}
+    static GLBuffer Create(unsigned int target, unsigned int usage_hint);
+	void Destroy();
+    bool Data(void* data, size_t sz);
+    bool Valid();
+    void Bind();
+private:
+    
+    unsigned int buffer;
+    unsigned int target;
+    unsigned int usage;
+};
+
+}
+}
+
+#endif
