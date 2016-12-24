a7c905961d0a3d19194bff3aa99674c563a24682
megamilkx <gyrocopter3k@gmail.com>
2016-12-24 21:17:32 +0300

    Finish up GFX::Mesh (still needs some work)

---
+++ src/gfx/mesh.cpp
@@ -0,0 +1,148 @@
+#include "mesh.h"
+
+#include <map>
+
+namespace Au{
+namespace GFX{
+
+// Global attribute index
+typedef unsigned int AttribIndex;
+// A number that tells you which one of the same attribute type
+// of the same vertex format this is
+typedef unsigned int AttribInstance;
+    
+std::map<std::pair<typeindex, AttribInstance>, AttribIndex> globalAttribIndexMap;
+AttribIndex GetGlobalAttribIndex(typeindex type, unsigned int instance)
+{
+    std::pair<typeindex, AttribInstance> key = std::make_pair(type, instance);
+    std::map<std::pair<typeindex, AttribInstance>, AttribIndex>::iterator it;
+    it = globalAttribIndexMap.find(key);
+    if(it == globalAttribIndexMap.end())
+    {
+        AttribIndex new_index = globalAttribIndexMap.size();
+        globalAttribIndexMap[key] = new_index;
+        return new_index;
+    }
+    else
+    {
+        return it->second;
+    }
+}
+    
+std::map<typeindex, GLenum> MakeGLTypeMap()
+{
+    std::map<typeindex, GLenum> typemap;
+    typemap[TypeInfo<char>::Index()] = GL_BYTE;
+    typemap[TypeInfo<unsigned char>::Index()] = GL_UNSIGNED_BYTE;
+    typemap[TypeInfo<short>::Index()] = GL_SHORT;
+    typemap[TypeInfo<unsigned short>::Index()] = GL_UNSIGNED_SHORT;
+    typemap[TypeInfo<int>::Index()] = GL_INT;
+    typemap[TypeInfo<unsigned int>::Index()] = GL_UNSIGNED_INT;
+    // GL_HALF_FLOAT
+    typemap[TypeInfo<float>::Index()] = GL_FLOAT;
+    typemap[TypeInfo<double>::Index()] = GL_DOUBLE;
+    // GL_FIXED
+    // GL_INT_2_10_10_10_REV
+    // GL_UNSIGNED_INT_2_10_10_10_REV
+    // GL_UNSIGNED_INT_10F_11F_11F_REV
+    
+    return typemap;
+}
+    
+GLenum GetGLType(typeindex index)
+{
+    static std::map<typeindex, GLenum> typemap = MakeGLTypeMap();
+    return typemap[index];
+}
+
+Mesh::Mesh()
+: vertexSize(0)
+{
+    glGenVertexArrays(1, &vao);
+    Bind();
+    vertexBuffer = GLBuffer::Create(GL_ARRAY_BUFFER, GL_STATIC_DRAW);
+    vertexBuffer.Bind();
+    indexBuffer = GLBuffer::Create(GL_ELEMENT_ARRAY_BUFFER, GL_STATIC_DRAW);
+    indexBuffer.Bind();
+}
+
+Mesh::~Mesh()
+{
+    glDeleteVertexArrays(1, &vao);
+    vertexBuffer.Destroy();
+    indexBuffer.Destroy();
+}
+
+void Mesh::Format(const std::vector<AttribInfo>& vertexFormat)
+{
+    this->vertexFormat = vertexFormat;
+    vertexSize = 0;
+    for(unsigned int i = 0; i < vertexFormat.size(); ++i)
+    {
+        vertexSize += vertexFormat[i].elemSize * vertexFormat[i].elemCount;
+    }
+    
+    Bind();
+    
+    size_t offset = 0;
+    std::map<typeindex, AttribInstance> instanceCounters;
+    for(unsigned int i = 0; i < vertexFormat.size(); ++i)
+    {
+        const AttribInfo& attrInfo = vertexFormat[i];
+        AttribIndex globalAttribIndex = 
+            GetGlobalAttribIndex(attrInfo.typeIndex,
+                                 instanceCounters[attrInfo.typeIndex]++);
+        glVertexAttribPointer(globalAttribIndex,                    //*attrib index
+                              attrInfo.elemCount,                   //*attrib element count
+                              GetGLType(attrInfo.elemType),         //*element type
+                              GL_FALSE,                             //*normalize?
+                              vertexSize,                           //*stride
+                              (void*)offset);                       //*offset
+        offset += attrInfo.elemSize * attrInfo.elemCount;
+        glEnableVertexAttribArray(globalAttribIndex);
+    }
+}
+
+void Mesh::VertexData(void* data, unsigned int count)
+{
+    if(vertexSize == 0)
+        return;
+    
+    vertexBuffer.Data(data, vertexSize * count);
+}
+
+void Mesh::IndexData(const std::vector<unsigned short>& data)
+{
+    IndexData((void*)data.data(), data.size());
+}
+
+void Mesh::IndexData(void* data, unsigned int count)
+{
+    if(count == 0)
+        return;
+    
+    indexBuffer.Data(data, sizeof(unsigned short) * count);
+    indexCount = count;
+    //index_type = GL_UNSIGNED_SHORT;
+}
+
+void Mesh::Bind()
+{
+    glBindVertexArray(vao);
+}
+
+void Mesh::Render()
+{
+    glDrawElements(GL_TRIANGLES, indexCount, GL_UNSIGNED_SHORT, 0);
+}
+
+void Mesh::PrintFormat()
+{
+    for(unsigned int i = 0; i < vertexFormat.size(); ++i)
+    {
+        vertexFormat[i].Print();
+    }
+}
+ 
+}
+}
--- src/gfx/mesh.h
+++ src/gfx/mesh.h
@@ -7,14 +7,27 @@
 
 #include "gl/glbuffer.h"
 
+#include "../util/typeinfo.h"
+
 namespace Au{
 namespace GFX{
     
 struct AttribInfo
 {
+    typeindex typeIndex;
     std::string name;
-    unsigned int elemType;
+    typeindex elemType;
+    unsigned char elemSize;
     unsigned char elemCount;
+    
+    void Print()
+    {
+        std::cout << name << ": " << std::endl;
+        std::cout << "typeIndex: " << typeIndex << std::endl;
+        std::cout << "elemType: " << elemType << std::endl;
+        std::cout << "elemSize: " << (int)elemSize << std::endl;
+        std::cout << "elemCount: " << (int)elemCount << std::endl;
+    }
 };
     
 class AttributeBase
@@ -34,11 +47,14 @@ class Attribute : public AttributeBase
 {
 public:
     Attribute() {}
-    Attribute(std::string name) : name(name) {}
+    Attribute(std::string name, typeindex typeIndex)
+    : name(name), myTypeIndex(typeIndex) {}
     operator AttribInfo() const
     {
         AttribInfo info;
-        info.elemType = 0;
+        info.typeIndex = myTypeIndex;
+        info.elemType = TypeInfo<T>::Index();
+        info.elemSize = sizeof(T);
         info.elemCount = count;
         info.name = name;
         return info;
@@ -54,6 +70,7 @@ public:
 protected:
     T value[count];
     std::string name;
+    typeindex myTypeIndex;
 };
 
 #define DEFINE_MESH_ATTRIB(NAME, TYPE, COUNT) \
@@ -61,7 +78,7 @@ class NAME : public Attribute<TYPE, COUNT> \
 { \
 public: \
     NAME() \
-    : Attribute<TYPE, COUNT>(#NAME){} \
+    : Attribute<TYPE, COUNT>(#NAME, TypeInfo<NAME>::Index()){} \
 }
 
 DEFINE_MESH_ATTRIB(Position, float, 3);
@@ -73,25 +90,35 @@ DEFINE_MESH_ATTRIB(UV, float, 2);
 class Mesh
 {
 public:
-    void Format(const std::vector<AttribInfo>& vertexFormat)
-    {
-        this->vertexFormat = vertexFormat;
-    }
+    Mesh();
+    ~Mesh();
     
-    void PrintFormat()
-    {
-        for(unsigned int i = 0; i < vertexFormat.size(); ++i)
-        {
-            std::cout << "Attrib: " << std::endl;
-            std::cout << vertexFormat[i].name << std::endl;
-            std::cout << (int)(vertexFormat[i].elemCount) << std::endl;
-        }
-    }
+    void Format(const std::vector<AttribInfo>& vertexFormat);
+    template<typename VERTEX>
+    void VertexData(const std::vector<VERTEX>& data);
+    void VertexData(void* data, unsigned int count);
+    void IndexData(const std::vector<unsigned short>& data);
+    void IndexData(void* data, unsigned int count);
+    
+    void Bind();
+    
+    void Render();
+    
+    void PrintFormat();
 private:
     std::vector<AttribInfo> vertexFormat;
+    size_t vertexSize;
+    unsigned int vao;
     GLBuffer vertexBuffer;
     GLBuffer indexBuffer;
+    unsigned int indexCount;
 };
+
+template<typename VERTEX>
+void Mesh::VertexData(const std::vector<VERTEX>& data)
+{
+    VertexData((void*)data.data(), data.size());
+}
 
 }
 }
