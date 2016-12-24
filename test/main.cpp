9cd4c33c9fc28f32fece6e53af6959d1cbac9087
megamilkx <gyrocopter3k@gmail.com>
2016-12-24 21:17:45 +0300

    Update test

--- test/main.cpp
+++ test/main.cpp
@@ -36,36 +36,69 @@ public:
     }
 };
 
+#pragma pack(push, 1)
+struct Vertex
+{
+    float x, y, z;
+    unsigned char r, g, b;
+};
+#pragma pack(pop)
+
 int main()
 {
     Au::Window window;
 
     //InputHandler input_handler(window);
-    
+
     Au::GFX::Device gfx_device;
     gfx_device.Init(window);
-    
-    Au::GFX::Mesh mesh;// = gfx_device.CreateMesh();
-    mesh.Format(Au::GFX::Position() <<
-                Au::GFX::Normal() <<
-                Au::GFX::ColorRGB());
-    mesh.PrintFormat();
-    //mesh.VertexData(vertices, count);
-    
-    Au::GFX::Shader shader;
-    shader.AddStage(Au::GFX::Shader::PIXEL, R"(
+
+    // Setting up a mesh ============================================
+
+    std::vector<Vertex> vertices =
+    { {-0.5f, -0.5f, 0.5f, 255, 50, 255},
+      {0.5f, -0.5f, 0.5f, 255, 50, 255},
+      {0.5f, 0.5f, 0.5f, 255, 50, 255},
+      {-0.5f, 0.5f, 0.5f, 255, 50, 255},
+      {-0.5f, -0.5f, -0.5f, 255, 50, 255},
+      {0.5f, -0.5f, -0.5f, 255, 50, 255},
+      {0.5f, 0.5f, -0.5f, 255, 50, 255},
+      {-0.5f, 0.5f, -0.5f, 255, 50, 255} };
+
+    std::vector<unsigned short> indices =
+    { 0, 1, 2, 2, 3, 0,
+      3, 2, 6, 6, 7, 3,
+      7, 6, 5, 5, 4, 7,
+      4, 0, 3, 3, 7, 4,
+      0, 1, 5, 5, 4, 0,
+      1, 5, 6, 6, 2, 1 };
+
+    Au::GFX::Mesh* mesh = gfx_device.CreateMesh();
+    mesh->Format(Au::GFX::Position() << Au::GFX::ColorRGB());
+    mesh->PrintFormat();
+    mesh->VertexData(vertices);
+    mesh->IndexData(indices);
+
+    gfx_device.Bind(mesh);
+
+    // ==============================================================
+
+    Au::GFX::Shader* shader = gfx_device.CreateShader();
+    shader->AddStage(Au::GFX::Shader::PIXEL, R"(
         void main()
         {
             gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
         })");
-    shader.AddStage(Au::GFX::Shader::VERTEX, R"(
+    shader->AddStage(Au::GFX::Shader::VERTEX, R"(
         void main()
         {
             gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
         })");
-    shader.Compile();
-    std::cout << shader.StatusString();
+    shader->Compile();
+    std::cout << shader->StatusString();
 
+    //gfx_device.Bind(shader);
+    
     window.Name("GAME");
     window.Resize(640, 480);
 
@@ -77,7 +110,9 @@ int main()
             gfx_device.Render();
             gfx_device.SwapBuffers();
         }
-        
+
+    gfx_device.Destroy(mesh);
+    gfx_device.Destroy(shader);
     gfx_device.Cleanup();
 
     return 0;
