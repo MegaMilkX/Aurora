13f9a6f0be776ff5c9530366d4d6d0b194ad2cae
megamilkx <gyrocopter3k@gmail.com>
2017-01-08 22:23:31 +0300

    Update wip

--- wip/src/octree.h
+++ wip/src/octree.h
@@ -1,12 +1,49 @@
-#ifndef QUADTREE_H
-#define QUADTREE_H
+#ifndef OCTREE_H
+#define OCTREE_H
 
-template<typename T, typename UNIT = float>
+namespace Au{
+
+class Object
+{
+    
+};
+
+class Cell
+{
+public:
+    bool Insert(const Object& object)
+    {
+        
+    }
+private:
+    Math::Vec3f pos;
+    Math::Vec3f size;
+    Cell* children[8];
+};
+
 class Octree
 {
 public:
-    void Insert(UNIT x, UNIT y, UNIT z, const T& value);
-    void Erase(UNIT x, UNIT y, UNIT z);
+    void Insert(const Object& object)
+    {
+        while(!rootCell->Insert(object))
+            Expand(object);
+    }
+    void Expand(const Object& object)
+    {
+        
+    }
+private:
+    Cell* rootCell;
 };
+
+int main()
+{
+    Octree octree;
+    
+    return 0;
+}
+
+}
 
 #endif
