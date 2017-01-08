cea0f10a7e133d4e42792a70a9491ce01e264a73
megamilkx <gyrocopter3k@gmail.com>
2017-01-08 22:20:23 +0300

    Add temporary Input::LoadDeviceList()

--- src/input/handler.h
+++ src/input/handler.h
@@ -22,6 +22,51 @@ public:
     virtual void OnUp(const Event& e);
     virtual void OnMouseMove(const Event& e);
 };
+
+void LoadDeviceList()
+{
+    UINT nDevices;
+    PRAWINPUTDEVICELIST pRawInputDeviceList;
+    
+    if(GetRawInputDeviceList(0, &nDevices, sizeof(RAWINPUTDEVICELIST)) != 0)
+        return;
+    
+    std::cout << "Input device count: " << nDevices << std::endl;
+    
+    pRawInputDeviceList = new RAWINPUTDEVICELIST[nDevices];
+    
+    if(GetRawInputDeviceList(pRawInputDeviceList, &nDevices, sizeof(RAWINPUTDEVICELIST)) == -1)
+    {
+        delete pRawInputDeviceList;
+        return;
+    }
+    
+    for(UINT i = 0; i < nDevices; ++i)
+    {
+        UINT dataSize = 0;
+        GetRawInputDeviceInfo(pRawInputDeviceList[i].hDevice, 
+                              RIDI_DEVICENAME,
+                              NULL,
+                              &dataSize);
+                              
+        wchar_t* data = new wchar_t[dataSize];
+
+        GetRawInputDeviceInfo(pRawInputDeviceList[i].hDevice,
+                              RIDI_DEVICENAME,
+                              (void*)data,
+                              &dataSize);
+        
+        if(dataSize > 0)
+        {
+        std::wstring deviceName(data, data + dataSize);
+        std::wcout << deviceName << std::endl;
+        // TODO
+        }
+        delete data;
+    }
+    
+    delete pRawInputDeviceList;
+}
   
 }}
 
