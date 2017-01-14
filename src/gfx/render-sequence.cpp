0dd23f45e57aaeda7e7a48a472d3216029d3faf3
megamilkx <gyrocopter3k@gmail.com>
2017-01-14 14:31:20 +0300

    Add more render sequence operators, most of them empty

--- src/gfx/render-sequence.cpp
+++ src/gfx/render-sequence.cpp
@@ -3,19 +3,6 @@
 namespace Au{
 namespace GFX{
 
-RenderSequence& RenderSequence::operator<<(RenderState* state)
-{
-    steps.push_back(Step(BIND_RENDERSTATE, state));
-    return *this;
-}
-
-RenderSequence& RenderSequence::operator<<(Mesh* mesh)
-{
-    steps.push_back(Step(BIND_MESH, mesh));
-    steps.push_back(Step(RENDER_MESH, mesh));
-    return *this;
-}
-
 void RenderSequence::Execute(Device* device)
 {
     for(unsigned i = 0; i < steps.size(); ++i)
@@ -29,12 +16,56 @@ void RenderSequence::Execute(Device* device)
         case BIND_MESH:
             device->Bind((Mesh*)(steps[i].data));
             break;
-        case RENDER_MESH:
+        case RENDER:
             device->Render();
             break;
         }
     }
 }
+
+RenderSequence& RenderSequence::operator<<(RenderState* state)
+{
+    steps.push_back(Step(BIND_RENDERSTATE, state));
+    return *this;
+}
+
+RenderSequence& RenderSequence::operator<<(Mesh* mesh)
+{
+    steps.push_back(Step(BIND_MESH, mesh));
+    steps.push_back(Step(RENDER));
+    return *this;
+}
+
+RenderSequence& operator<<(const char* uniformName)
+{
+    cachedUniformName = std::string(uniformName);
+    return *this;
+}
+
+RenderSequence& operator<<(const std::string& uniformName)
+{
+    cachedUniformName = uniformName;
+    return *this;
+}
+
+RenderSequence& operator<<(float uniform)
+{
+    
+}
+
+RenderSequence& operator<<(const Math::Vec2f& uniform);
+RenderSequence& operator<<(const Math::Vec3f& uniform);
+RenderSequence& operator<<(const Math::Vec4f& uniform);
+RenderSequence& operator<<(int uniform);
+RenderSequence& operator<<(const Math::Vec2i& uniform);
+RenderSequence& operator<<(const Math::Vec3i& uniform);
+RenderSequence& operator<<(const Math::Vec4i& uniform);
+RenderSequence& operator<<(unsigned int uniform);
+RenderSequence& operator<<(const Math::Vec2ui& uniform);
+RenderSequence& operator<<(const Math::Vec3ui& uniform);
+RenderSequence& operator<<(const Math::Vec4ui& uniform);
+RenderSequence& operator<<(const Math::Mat3f& uniform);
+RenderSequence& operator<<(const Math::Mat4f& uniform);
    
 }
 }
--- src/gfx/render-sequence.h
+++ src/gfx/render-sequence.h
@@ -16,7 +16,21 @@ public:
         NO_CMD,
         BIND_MESH,
         BIND_RENDERSTATE,
-        RENDER_MESH
+        RENDER,
+        UNIFORM_FLOAT,
+        UNIFORM_FLOAT2,
+        UNIFORM_FLOAT3,
+        UNIFORM_FLOAT4,
+        UNIFORM_INT,
+        UNIFORM_INT2,
+        UNIFORM_INT3,
+        UNIFORM_INT4,
+        UNIFORM_UINT,
+        UNIFORM_UINT2,
+        UNIFORM_UINT3,
+        UNIFORM_UINT4,
+        UNIFORM_MAT3F,
+        UNIFORM_MAT4F
     };
     
     struct Step
@@ -29,12 +43,29 @@ public:
         void* data;
     };
     
+    void Execute(Device* device);
+    
     RenderSequence& operator<<(RenderState* state);
     RenderSequence& operator<<(Mesh* mesh);
-    
-    void Execute(Device* device);
+    RenderSequence& operator<<(const char* uniformName);
+    RenderSequence& operator<<(const std::string& uniformName);
+    RenderSequence& operator<<(float uniform);
+    RenderSequence& operator<<(const Math::Vec2f& uniform);
+    RenderSequence& operator<<(const Math::Vec3f& uniform);
+    RenderSequence& operator<<(const Math::Vec4f& uniform);
+    RenderSequence& operator<<(int uniform);
+    RenderSequence& operator<<(const Math::Vec2i& uniform);
+    RenderSequence& operator<<(const Math::Vec3i& uniform);
+    RenderSequence& operator<<(const Math::Vec4i& uniform);
+    RenderSequence& operator<<(unsigned int uniform);
+    RenderSequence& operator<<(const Math::Vec2ui& uniform);
+    RenderSequence& operator<<(const Math::Vec3ui& uniform);
+    RenderSequence& operator<<(const Math::Vec4ui& uniform);
+    RenderSequence& operator<<(const Math::Mat3f& uniform);
+    RenderSequence& operator<<(const Math::Mat4f& uniform);
 private:
     std::vector<Step> steps;
+    std::string cachedUniformName;
 };
 
 }
