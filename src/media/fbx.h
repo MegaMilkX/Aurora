fe25023910ee4ae0ce65c2b1040239c5d1adf5a0
megamilkx <gyrocopter3k@gmail.com>
2017-01-17 00:20:29 +0300

    Add fbx reader mockup

--- src/media/fbx.h
+++ src/media/fbx.h
@@ -1,11 +1,27 @@
 #ifndef FBX_H
 #define FBX_H
 
+#include <vector>
+
 namespace Au{
 namespace Media{
 namespace FBX{
 
 bool Read(const char* data, unsigned size);
+
+class Reader
+{
+public:
+    bool Read(const char* data, unsigned size);
+    template<typename T>
+    std::vector<T> GetArray(unsigned int attribute);
+};
+
+template<typename T>
+std::vector<T> Reader::GetArray(unsigned int attribute)
+{
+    
+}
 
 }
 }
