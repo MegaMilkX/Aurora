0f9f07729a38324e0e89d2653b0dbf69cb683bc6
megamilkx <gyrocopter3k@gmail.com>
2017-01-20 02:05:12 +0300

    Add prop class for fbx handling

--- src/media/fbx.h
+++ src/media/fbx.h
@@ -1,6 +1,7 @@
 #ifndef FBX_H
 #define FBX_H
 
+#include <iostream>
 #include <string>
 #include <vector>
 
@@ -12,6 +13,71 @@ typedef uint8_t Byte;
 typedef uint32_t Word;
 typedef uint64_t DoubleWord;
 
+class Prop
+{
+public:
+    void Print(unsigned level = 0)
+    {
+        for(unsigned i = 0; i < level; ++i)
+            std::cout << "  ";
+        
+        switch(type)
+        {
+        // 16 bit int
+        case 'Y':
+            std::cout << *(int16_t*)(data.data());
+            break;
+        // 1 bit bool
+        case 'C':
+            std::cout << "1 bit bool";
+            break;
+        // 32 bit int
+        case 'I':
+            std::cout << *(int32_t*)(data.data());
+            break;
+        case 'F':
+            std::cout << *(float*)(data.data());
+            break;
+        // double
+        case 'D':
+            std::cout << *(double*)(data.data());
+            break;
+        // 64 bit int
+        case 'L':
+            std::cout << *(int64_t*)(data.data());
+            break;
+        // Binary data
+        case 'R':
+            std::cout << "Just binary data";
+            break;
+        case 'b':
+            break;
+        case 'f':
+            
+        case 'i':
+        case 'd':
+        case 'l':
+            std::cout << "Some array";
+            break;
+        case 'S':
+            std::cout << std::string(data.data(), data.data() + data.size());
+            break;
+        }
+        
+        std::cout << std::endl;
+    }
+    void Type(char type)
+    {
+        this->type = type;
+    }
+    void Data(const std::vector<char>& data)
+    {
+        this->data = data;
+    }
+private:
+    char type;
+    std::vector<char> data;
+};
 
 class Node
 {
