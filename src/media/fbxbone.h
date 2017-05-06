7949c515639ae21ffe24c2fddc4a7f95341fd929
megamilkx <gyrocopter3k@gmail.com>
2017-05-06 16:25:31 +0300

    Move vector and matrix conversion to fbxutil

--- src/media/fbxbone.h
+++ src/media/fbxbone.h
@@ -5,79 +5,11 @@
 
 #include "fbxnode.h"
 
-#include "fbxsettings.h"
+#include "fbxutil.h"
 
 namespace Au{
 namespace Media{
 namespace FBX{
-    
-inline bool IsSameAxisDirection(Axis a, Axis b)
-{
-    if(a == AXIS_X && b == AXIS_X)
-        return true;
-    else if(a == AXIS_Y && b == AXIS_Y)
-        return true;
-    else if(a == AXIS_Z && b == AXIS_Z)
-        return true;
-    return false;
-}
-    
-inline bool IsSameAxis(Axis a, Axis b)
-{
-    if((a == AXIS_X || a == AXIS_MX) &&
-       (b == AXIS_X || b == AXIS_MX))
-        return true;
-    else if((a == AXIS_Y || a == AXIS_MY) &&
-       (b == AXIS_Y || b == AXIS_MY))
-       return true;
-    else if((a == AXIS_Z || a == AXIS_MZ) &&
-       (b == AXIS_Z || b == AXIS_MZ))
-       return true;
-    else
-        return false;
-}
-    
-inline void GetConvertedAxisIndices(Settings& settings, unsigned (&indices)[3], float (&mults)[3])
-{
-    Axis origAxes[3] = { 
-        settings.origAxes.right,
-        settings.origAxes.up,
-        settings.origAxes.front
-    };
-    Axis convAxes[3] = {
-        settings.convAxes.right,
-        settings.convAxes.up,
-        settings.convAxes.front
-    };
-    
-    for(unsigned i = 0; i < 3; ++i)
-    {
-        for(unsigned j = 0; j < 3; ++j)
-        {
-            if(IsSameAxis(origAxes[i], convAxes[j]))
-            {
-                indices[i] = j;
-                if(!IsSameAxisDirection(origAxes[i], convAxes[j]))
-                    mults[i] = -1.0f;
-            }
-        }
-    }
-}
-    
-inline void ConvertVector(Settings& settings, Au::Math::Vec3f& vec)
-{
-    unsigned indices[3];
-    float multipliers[3] = { 1.0f, 1.0f, 1.0f };
-    
-    GetConvertedAxisIndices(settings, indices, multipliers);
-    
-    float newVec[3];
-    for(unsigned i = 0; i < 3; ++i)
-        newVec[indices[i]] = vec[i] * multipliers[i];
-    vec.x = newVec[0];
-    vec.y = newVec[1];
-    vec.z = newVec[2];
-}
 
 class Bone
 {
@@ -102,7 +34,7 @@ public:
         Au::Math::Quat qrot(0.0f, 0.0f, 0.0f, 1.0f);
         Au::Math::Vec3f scale(1.0f, 1.0f, 1.0f);
         
-        if(deformer)
+        if(false/*deformer*/)
         {
             std::vector<double> mat =
                 deformer->Get("TransformLink")[0].GetArray<double>();
@@ -114,7 +46,9 @@ public:
             
             Au::Math::Mat4f mat4f = *(Math::Mat4f*)matf.data();
             
-            pos = Au::Math::Vec3f(matf[12], matf[13], matf[14]);
+            ConvertMatrix(settings, mat4f);
+            
+            pos = Au::Math::Vec3f(mat4f[3].x, mat4f[3].y, mat4f[3].z);
             qrot = Au::Math::ToQuat(ToMat3(mat4f));
             
             //ConvertVector(settings, pos);
@@ -134,13 +68,15 @@ public:
             ConvertVector(settings, pos);
             
             Node& lclRot = node.Get("Properties70").GetWhere(0, "Lcl Rotation");
-            rot.x = (float)lclRot[4].GetDouble();
-            rot.y = (float)lclRot[5].GetDouble();
-            rot.z = (float)lclRot[6].GetDouble();
+            rot.x = ((float)lclRot[4].GetDouble() * Au::Math::PI) / 180.0f;
+            rot.y = ((float)lclRot[5].GetDouble() * Au::Math::PI) / 180.0f;
+            rot.z = ((float)lclRot[6].GetDouble() * Au::Math::PI) / 180.0f;
+            
+            ConvertVector(settings, rot);
             
             transform = 
                 Au::Math::Translate(Au::Math::Mat4f(1.0f), pos) * 
-                Au::Math::ToMat4(EulerToQuat(rot)) * 
+                Au::Math::ToMat4(Au::Math::EulerToQuat(rot)) * 
                 Au::Math::Scale(Au::Math::Mat4f(1.0f), scale);
         }
         
