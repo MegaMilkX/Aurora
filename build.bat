ab392c38c90e2493d4987617c8c6dcb9313ea897
megamilkx <gyrocopter3k@gmail.com>
2017-05-15 21:18:04 +0300

    Update build script

--- build.bat
+++ build.bat
@@ -40,11 +40,22 @@ for /L %%i in (1,1,%ARG_COUNT%) do (
         set BUILD_TYPE=lib
     ) else if !ARGS[%%i]! EQU dll (
         set BUILD_TYPE=dll
+    ) else if !ARGS[%%i]! EQU x86 (
+        set "ARCHITECTURE="
+        set "LINKARCH=X86"
+    ) else if !ARGS[%%i]! EQU x64 (
+        set "ARCHITECTURE=amd64"
+        set "LINKARCH=X64"
     )
 )
 
 if not defined BUILD_TYPE (
     set BUILD_TYPE=exe
+)
+
+if not defined ARCHITECTURE (
+    set "ARCHITECTURE="
+    set "LINKARCH=X86"
 )
 
 set BUILDDIRNAME=build
@@ -75,7 +86,7 @@ if not defined VCVARSALLPATH (
     exit /b 1
 )
 
-if not defined DevEnvDir (call %VCVARSALLPATH%)
+if not defined DevEnvDir (call %VCVARSALLPATH% %ARCHITECTURE%)
 
 REM =============================================
 
@@ -109,9 +120,8 @@ if %BUILD_TYPE% EQU exe (
     link /OUT:"..\%BUILDDIR%\%EXENAME%.exe" ^
     %OBJS% ^
     %LIBRARIES% ^
-    /MACHINE:X86 ^
+    /MACHINE:%LINKARCH% ^
     /OPT:REF ^
-    /SAFESEH ^
     /OPT:ICF ^
     /ERRORREPORT:PROMPT ^
     /NOLOGO ^
@@ -124,9 +134,8 @@ if %BUILD_TYPE% EQU exe (
     link /DLL /OUT:"..\%BUILDDIR%\%EXENAME%.dll" ^
     %OBJS% ^
     %LIBRARIES% ^
-    /MACHINE:X86 ^
+    /MACHINE:%LINKARCH% ^
     /OPT:REF ^
-    /SAFESEH ^
     /OPT:ICF ^
     /ERRORREPORT:PROMPT ^
     /NOLOGO ^
